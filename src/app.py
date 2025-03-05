10fabeb2d4dea09716f1dc8c4e982bcda1847af2
benjaminlutz <benlutz@pdx.edu>
2025-03-05 11:07:31 -0800

    llm start

--- src/app.py
+++ src/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from controllers.main_controller import main_bp
+from models.llm import run_llm_agents
 
 app = Flask(__name__)
 app.register_blueprint(main_bp)
 
 if __name__ == '__main__':
+    run_llm_agents()
     app.run(debug=True)

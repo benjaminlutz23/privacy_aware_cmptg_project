ea35f8f0a406b1e67a7f5e476366ca5cab64d4be
benjaminlutz <benlutz@pdx.edu>
2025-03-08 19:13:06 -0800

    web app fully function at this point

--- src/controllers/main_controller.py
+++ src/controllers/main_controller.py
@@ -13,7 +13,20 @@ def process():
     # icons = assign_privacy_icons(data['policy'])
     return
 
-@main_bp.route('/view_policy/<source>')
-def view_policy(source):
+@main_bp.route('/get_policies/<source>')
+def get_policies(source):
     policy_dir = os.path.join(os.getcwd(), 'src', 'data', 'llm_annotated_policies', source)
-    return send_from_directory(policy_dir, '20_theatlantic.com.html')
+    policies = []
+    for filename in os.listdir(policy_dir):
+        if filename.endswith('.html'):
+            policy_name = filename.replace('.html', '').split('_', 1)[-1]
+            policies.append(policy_name)
+    return jsonify({'policies': policies})
+
+@main_bp.route('/view_policy/<source>/<policy>')
+def view_policy(source, policy):
+    policy_dir = os.path.join(os.getcwd(), 'src', 'data', 'llm_annotated_policies', source)
+    for filename in os.listdir(policy_dir):
+        if filename.endswith('.html') and filename.split('_', 1)[-1].replace('.html', '') == policy:
+            return send_from_directory(policy_dir, filename)
+    return "Policy not found", 404

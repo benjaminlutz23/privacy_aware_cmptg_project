e2e9d3ecb6bc39755423f62dd72e69595ea08ab9
benjaminlutz <benlutz@pdx.edu>
2025-03-08 18:56:35 -0800

    web app can display a single annotation file

--- src/app.py
+++ src/app.py
@@ -1,13 +1,18 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from controllers.main_controller import main_bp
 from ai_policy_annotation.llm import run_llm_agents
 from ai_policy_annotation.copy_policies import main as copy_policies
 from ai_policy_annotation.rag import initialize_rag_database
+import os
 
 app = Flask(__name__)
 app.register_blueprint(main_bp)
 
+@app.route('/privacy-icon-images/<path:filename>')
+def serve_images(filename):
+    return send_from_directory(os.path.join(app.root_path, 'data', 'privacy-icon-images'), filename)
+
 if __name__ == '__main__':
     # copy_policies()
-    run_llm_agents()
-    # app.run(debug=True)
+    # run_llm_agents()
+    app.run(debug=True)
--- src/controllers/main_controller.py
+++ src/controllers/main_controller.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template, request, jsonify
-# from ai_policy_annotation.llm import assign_privacy_icons
+from flask import Blueprint, render_template, request, jsonify, send_from_directory
+import os
 
 main_bp = Blueprint('main', __name__)
 
@@ -12,3 +12,8 @@ def process():
     data = request.json
     # icons = assign_privacy_icons(data['policy'])
     return
+
+@main_bp.route('/view_policy/<source>')
+def view_policy(source):
+    policy_dir = os.path.join(os.getcwd(), 'src', 'data', 'llm_annotated_policies', source)
+    return send_from_directory(policy_dir, '20_theatlantic.com.html')
